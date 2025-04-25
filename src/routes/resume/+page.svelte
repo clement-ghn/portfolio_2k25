1b18274f963e1e4d307fa18a7a1657d4f072584d
clément Géhin <clement.gehin@4murs.fr>
2025-04-25 16:05:30 +0200

    fix du cv

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,37 +1,40 @@
 <script>
+	import { base } from '$app/paths'; // Import base path from $app/paths
 	import { data, title } from '@data/resume';
-
 	import Chip from '$lib/components/Chip/Chip.svelte';
 	import CommonPage from '$lib/components/CommonPage.svelte';
-</script>
-
-<CommonPage {title}>
+  </script>
+  
+  <CommonPage {title}>
 	<div class="resume">
-		{#if data}
-		<a href="/CV_2025.pdf" download>
-			<Chip size={'1.25em'}>Télécharger</Chip>
-		</a>		
-		{:else}
-			<Chip>Ooops! Pas encore de CV.</Chip>
-		{/if}
-	</div>
-</CommonPage>
-<footer class="text-center text-sm text-[var(--tertiary-text)] mt-10 p-4">
-	<p>
-		Template par <a href="https://github.com/RiadhAdrani" target="_blank" rel="noreferrer" class="text-[var(--accent-text)] underline">
-			Riadh Adrani
+	  {#if data}
+		<!-- Use $app/paths.base to correctly reference the file path in production -->
+		<a href={`${base}/CV_2025.pdf`} download>
+		  <Chip size={'1.25em'}>Télécharger</Chip>
 		</a>
+	  {:else}
+		<Chip>Ooops! Pas encore de CV.</Chip>
+	  {/if}
+	</div>
+  </CommonPage>
+  
+  <footer class="text-center text-sm text-[var(--tertiary-text)] mt-10 p-4">
+	<p>
+	  Template par <a href="https://github.com/RiadhAdrani" target="_blank" rel="noreferrer" class="text-[var(--accent-text)] underline">
+		Riadh Adrani
+	  </a>
 	</p>
-</footer>
-
-<style lang="scss">
+  </footer>
+  
+  <style lang="scss">
 	.resume {
-		display: flex;
-		justify-content: center;
-		margin-top: 20px;
-
-		& > a {
-			color: inherit;
-		}
+	  display: flex;
+	  justify-content: center;
+	  margin-top: 20px;
+  
+	  & > a {
+		color: inherit;
+	  }
 	}
-</style>
+  </style>
+  
