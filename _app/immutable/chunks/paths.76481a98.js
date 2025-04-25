5f0ccda97114a8cf9f06e2dc5324e0f23668bda3
clement-ghn <ent.clementgehin@gmail.com>
2025-04-25 14:37:23 +0000

    Deploying to gh-pages from @ clement-ghn/portfolio_2k25@dc640ec35315cfbef72c88c730534509bf61264d 🚀

---
+++ _app/immutable/chunks/paths.76481a98.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_z27m3j)==null?void 0:s.base)??"/portfolio_2k25";var t;const e=((t=globalThis.__sveltekit_z27m3j)==null?void 0:t.assets)??a;export{e as a,a as b};
--- _app/immutable/chunks/skills.c7bf44f3.js
+++ _app/immutable/chunks/skills.c7bf44f3.js
@@ -1,4 +1,4 @@
-import{A as e}from"./UIcon.5f987159.js";const u=`# Svelte
+import{A as e}from"./UIcon.217e8024.js";const u=`# Svelte
 
 ---
 
